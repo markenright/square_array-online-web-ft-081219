d84c411bd319fc3030d1b808efd3608153648b43
Mark Enright <enright.mark@gmail.com>
2019-08-14 18:00:38 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,24 @@
 def square_array(array)
   # your code here
+  new_array = []
+  array.each do |number|
+    new_array.push(number ** 2)
+  
+  end  
+  return new_array
+end
+
+
+
+def square_array2(array)
+  
+  new_array = []
+  array.collect do |number|
+    number ** 2
+  end
+  
+  
+  
+  
+  
 end
